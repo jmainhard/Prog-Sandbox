37cb7a2291a5cbf82809f0dff0a9ae2ffdce9827
Jorge Mainhard Bravo <63559099+jmainhard@users.noreply.github.com>
2020-09-08 15:16:11 -0300

    completed char parser
    esNum
    esLetra
    esPunt

--- CPractico3.java
+++ CPractico3.java
@@ -30,62 +30,73 @@ public class CPractico3 {
 
     }
 
-    public static int parser(char i) {
+    public static int parser(char car) {
         //letra 0 1
         //punt 2
         //num 3
         //otro 4
-        if (esLetra(i)) {
-            return mayuscParser(i); //ver documentación
-        } else if (esPunt(i)) {
+        if (esLetra(car)) {
+            return mayuscParser(car); //ver documentación
+        } else if (esPunt(car)) {
             return 2;
-        } else if (esNum(i)) {
+        } else if (esNum(car)) {
             return 3;
         } else {
             return 4;
         }
     }
 
-    public static boolean esLetra(char i) {
-        if (!esPunt && !esNum) {
-            return true;
-        } //TODO incluir caso de 'otro'
-        return true;
+    public static boolean esLetra(char car) {
+        //todo pasar char[] -> char[][]
+        char[] coleccion = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                            'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+                            'q', 'r', 's', 't', 'u', 'v', 'x', 'y',
+                            'z', 'ñ', 'w'};
+        
+        for (int i = 0; i < coleccion.length; i++) {
+            if (car == coleccion[i]) {
+                return true;
+            }
+        }
+        return false;
     }
     
-    public static int mayuscParser(char i) {
+    public static int mayuscParser(char car) {
+        
         return 1;
     }
 
-    public static boolean esPunt(char i) {
-        //todo, tener una matriz de chars de puntuación
+    public static boolean esPunt(char car) {
+        //array que funciona como colección de chars de puntuación
         //para identificar la presencia de alguno,
         //en caso contrario return false
         
-        if (i == '.' || i == ',') {
-            
-        } if (i == ';' || i == ':') {
-            
-        } if (i == '-' || i == '¿') {
-            
-        } if (i == '.' || i == ',') {
-            
-        } if (i == '.' || i == ',') {
-            
-        } if (i == '.' || i == ',') {
-            
+        char[] coleccion = {'.', ',', ';', ':', '-', '¿',
+                            '?', '¡', '!', '"', '(', ')'};
+        
+        for (int i = 0; i < coleccion.length; i++) {
+            if (car == coleccion[i]) {
+                return true;
+            }
         }
-        return true;
+        return false;
     }
 
-    public static boolean esNum(char i) {
-        return true;
+    public static boolean esNum(char car) {
+        char[] coleccion = {'1', '2', '3', '4', '5', '6',
+                            '6', '7', '8', '9', '0'};
+        for (int i = 0; i < 10; i++) {
+            if (car == coleccion[i]) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void traducir(char[] arr) {
         Libreria.printArray(arr);
         for (int i = 0; i < arr.length; i++) {
-            switch (parser(i)) {
+            switch (parser(arr[i])) {
                 case 0:
                     
                     break;
@@ -96,7 +107,23 @@ public class CPractico3 {
     }
 
     public static void main(String[] args) {
-        input();
+        char a = 'a';
+        char b = '?';
+        char c = '8';
+        System.out.println(a);
+        System.out.println("Es letra: "+ esLetra(a));
+        System.out.println("Es punt: "+ esPunt(a));
+        System.out.println("Es num: "+ esNum(a));
+        
+        System.out.println(b);
+        System.out.println("Es letra: "+ esLetra(b));
+        System.out.println("Es punt: "+ esPunt(b));
+        System.out.println("Es num: "+ esNum(b));
+        
+        System.out.println(c);
+        System.out.println("Es letra: "+ esLetra(c));
+        System.out.println("Es punt: "+ esPunt(c));
+        System.out.println("Es num: "+ esNum(c));
     }
 
 }
