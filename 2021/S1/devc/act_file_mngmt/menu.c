17b8ab3a1c9b7dec86e3cc66f63012fda980cce4
Jorge Mainhard Bravo <63559099+jmainhard@users.noreply.github.com>
2021-04-10 20:59:43 -0400

    Fix punteros rut, nombre

--- 2021/S1/devc/act_file_mngmt/menu.c
+++ 2021/S1/devc/act_file_mngmt/menu.c
@@ -19,40 +19,39 @@ struct persona
 };
 
 
-void menu() 
-{
-	unsigned short opcion = 0;
-	while (opcion >= 0 && opcion < 5) {
-	  printf("%s\n", "[1] - USD to EUR");
-	  printf("%s\n", "[2] - EUR to USD");
-	  printf("%s\n", "[3] - Area y perimetro");
-	  printf("%s\n", "[4] - Precio con descuento");
-	  printf("%s\n", "[5] - Salir");
-	  scanf("%hu", &opcion);
-	  
-	  switch (opcion) {
-		case 1:
-			usd_to_eur();
-			opcion = 0;
-			break;
-		case 2:
-			eur_to_usd();
-			opcion = 0;
-			break;
-		case 3:
-			calc_area_prmtro_rctlo();
-			break;
-		case 4:
-			calc_descuento();
-			break;
-		case 5:
-			return 0;
-		default:
-			opcion = 0;
-			break;
-	  }
-	}
-}
+// void menu() 
+// {
+// 	unsigned short opcion = 0;
+// 	while (opcion >= 0 && opcion < 5) {
+// 	  printf("%s\n", "[1] - USD to EUR");
+// 	  printf("%s\n", "[2] - EUR to USD");
+// 	  printf("%s\n", "[3] - Area y perimetro");
+// 	  printf("%s\n", "[4] - Precio con descuento");
+// 	  printf("%s\n", "[5] - Salir");
+// 	  scanf("%hu", &opcion);
+// 	  switch (opcion) {
+// 		case 1:
+// 			usd_to_eur();
+// 			opcion = 0;
+// 			break;
+// 		case 2:
+// 			eur_to_usd();
+// 			opcion = 0;
+// 			break;
+// 		case 3:
+// 			calc_area_prmtro_rctlo();
+// 			break;
+// 		case 4:
+// 			calc_descuento();
+// 			break;
+// 		case 5:
+// 			return 0;
+// 		default:
+// 			opcion = 0;
+// 			break;
+// 	  }
+// 	}
+// }
 
 void prueba_json()
 {
@@ -109,8 +108,8 @@ struct persona get_persona(int idx)
     json_object_object_get_ex(parsed_json, "nombre", &nombre);
     json_object_object_get_ex(parsed_json, "edad", &edad);
 
-    *personaOut.rut = json_object_get_string(rut);
-    *personaOut.nombre = json_object_get_string(nombre);
+    *personaOut.rut = *json_object_get_string(rut);
+    *personaOut.nombre = *json_object_get_string(nombre);
     personaOut.edad = json_object_get_int(edad);
 
     return personaOut;
@@ -122,9 +121,6 @@ void mostrar_persona(struct persona persona)
     printf("NOMBRE. %s\n", json_object_get_string(persona.nombre));
     printf("EDAD. %d\n", json_object_get_int(persona.edad));
 }
-
-
-
 
 int main(void) 
 {
