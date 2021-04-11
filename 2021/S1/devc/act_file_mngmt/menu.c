91f83ae2b9b2d4a051951bbc30772f8fbb5be7a7
Jorge Mainhard Bravo <63559099+jmainhard@users.noreply.github.com>
2021-04-11 00:52:39 -0400

    Update menu.c

--- 2021/S1/devc/act_file_mngmt/menu.c
+++ 2021/S1/devc/act_file_mngmt/menu.c
@@ -20,35 +20,38 @@ struct persona
 };
 
 
-// int menu() 
-// {
-// 	unsigned short opcion = 0;
-// 	while (opcion >= 0 && opcion < 5) {
-// 	  printf("%s\n", "[1] - Buscar por rut");
-// 	  printf("%s\n", "[2] - Cantidad de registros");
-// 	  printf("%s\n", "[3] - Promedio de las edades");
-// 	  printf("%s\n", "[4] - Mayor y menor edad");
-// 	  printf("%s\n", "[5] - Salir");
-// 	  scanf("%hu", &opcion);
-// 	  switch (opcion) {
-// 		case 1:
-// 			opcion = 0;
-// 			break;
-// 		case 2:
-// 			opcion = 0;
-// 			break;
-// 		case 3:
-// 			break;
-// 		case 4:
-// 			break;
-// 		case 5:
-// 			return 0;
-// 		default:
-// 			opcion = 0;
-// 			break;
-// 	  }
-// 	}
-// }
+int menu() 
+{
+	unsigned short opcion = 0;
+	while (opcion >= 0 && opcion < 5) {
+	  printf("%s\n", "[1] - Buscar por rut");
+	  printf("%s\n", "[2] - Cantidad de registros");
+	  printf("%s\n", "[3] - Promedio de las edades");
+	  printf("%s\n", "[4] - Mayor y menor edad");
+	  printf("%s\n", "[5] - Salir");
+	  scanf("%hu", &opcion);
+	  switch (opcion) {
+		case 1:
+            buscar_fn();
+			opcion = 0;
+			break;
+		case 2:
+            calc_cant_fn();
+			opcion = 0;
+			break;
+		case 3:
+            calc_prom_fn();
+			break;
+		case 4:
+			break;
+		case 5:
+			return 0;
+		default:
+			opcion = 0;
+			break;
+	  }
+	}
+}
 
 // deprecated
 void leer_json()
@@ -101,14 +104,11 @@ void buscar_fn()
 
     json_object *personas = json_object_object_get(root, "personas");
     n_personas = json_object_array_length(personas);
-    printf("%s\n", rut);
     for (int i = 0; i < n_personas; i++)
     {
         temp = json_object_array_get_idx(personas, i);
         json_object *rutObj = json_object_object_get(temp, "rut");
         rutTemp = strdup(json_object_get_string(rutObj)); 
-        printf("%s, ", rutTemp);
-        printf("%s\n", rut);
         if (strcmp(rutTemp,rut) == 0)
         {
             json_object *nameObj = json_object_object_get(temp, "nombre");
@@ -120,6 +120,41 @@ void buscar_fn()
             break;
         }
     }
+    json_object_put(root);
+}
+
+void calc_cant_fn()
+{
+    json_object *root = json_object_from_file("datos.json");
+    int n_personas;
+    json_object *personas = json_object_object_get(root, "personas");
+    n_personas = json_object_array_length(personas);
+    printf("\t\t\t%s %d\n", "Total registros:", n_personas);
+    json_object_put(root);
+}
+
+void calc_prom_fn()
+{
+    json_object *root, *temp;
+    int edad, n_personas;
+    float promEdad;
+
+    root = json_object_from_file("datos.json");
+    const char *rut = ask_rut();
+
+    json_object *personas = json_object_object_get(root, "personas");
+    n_personas = json_object_array_length(personas);
+    for (int i = 0; i < n_personas; i++)
+    {
+        temp = json_object_array_get_idx(personas, i);
+        json_object *ageObj = json_object_object_get(temp, "edad");
+        promEdad += json_object_get_int(ageObj);
+        break;
+    }
+
+    promEdad /= n_personas;
+    printf("\t\t\t%s %d\n", "Promedio de la edad:", promEdad);
+
     json_object_put(root);
 }
 
@@ -188,10 +223,9 @@ int main(void)
     // después de 3hrs y poco más intentando que ask_rut() me devolviera el string de forma-
     // correcta, abdico, he intentado todo lo que tengo a mi alcance pero-
     // no he podido.
-    // Cada vez que utilizo ask_rut() para conseguir el rut me lanza:
+    // Cada vez que utilizo ask_rut() para conseguir el rut desde fuera de buscar_fn() me lanza:
     // Segmentation fault (core dumped)
-    //  dejaré el rut hard codeado
-    // const char *rut = "24654848-k";
-    buscar_fn();
+    //  dejaré la invocación de ask_rut() dentro de buscar_fn() muy a mi pesar
+    menu();
     return 0;
 }
