3cf1e730ede020f45449d58178c25e126982ffe2
Jorge Mainhard Bravo <63559099+jmainhard@users.noreply.github.com>
2021-04-19 00:03:05 -0400

    Agenda init commit

---
+++ 2021/S1/devc/agenda/directorio.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <json-c/json.h>
+// estructura de datos para un directorio personal
+// 18-04-2021
+// Jorge M.
+
+void save_persona(const char *nombre, const char *direccion)
+{
+    const char *file_name = "datos_agenda.json";
+    const char *key = "per_";
+    json_object *root = json_object_from_file("datos.json");
+
+    strcat(key, nombre[0]);
+
+    json_object *contactos = json_object_object_get(root, key);
+    json_object *new_persona = json_object_new_object();
+
+    json_object_object_add(new_persona, "nombre", json_object_new_string(nombre));
+    json_object_object_add(new_persona, "direccion", json_object_new_string(direccion));
+    json_object_array_add(contactos, new_persona);
+
+    if (json_object_to_file_ext(file_name, root, JSON_C_TO_STRING_PRETTY)) 
+    {
+        printf("Error: no se pudo guardar %s!!\n", file_name);
+    }
+    else
+    {
+        printf("%s guardado.\n", file_name);
+    }
+    json_object_put(root); 
+}
+
+/*
+ *  Muestra el nombre y direccion de una persona, selecciona de arrays
+ *  agrupados por letra
+ *
+ *  @param idx indice de la persona a ser mostrada
+ *  @param letra primera letra del 'objeto' a ser buscado
+ */
+void show_persona(int idx, char *letra)
+{
+
+}
+
+int main(void)
+{
+    save_persona("Jorge Mainhard", "Avenida siempre viva 740");
+    return 0;
+}
