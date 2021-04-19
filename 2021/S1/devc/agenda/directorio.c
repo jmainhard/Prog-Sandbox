865e7ead25757c86df190cacfcfc4ab6d7d57355
Jorge Mainhard Bravo <63559099+jmainhard@users.noreply.github.com>
2021-04-19 00:09:33 -0400

    Fix included string.h fix strcat

--- 2021/S1/devc/agenda/directorio.c
+++ 2021/S1/devc/agenda/directorio.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <json-c/json.h>
+#include <string.h>
 // estructura de datos para un directorio personal
 // 18-04-2021
 // Jorge M.
@@ -8,9 +9,10 @@ void save_persona(const char *nombre, const char *direccion)
 {
     const char *file_name = "datos_agenda.json";
     const char *key = "per_";
+    const char *suffix = nombre[0]; 
     json_object *root = json_object_from_file("datos.json");
 
-    strcat(key, nombre[0]);
+    strcat(key, suffix);
 
     json_object *contactos = json_object_object_get(root, key);
     json_object *new_persona = json_object_new_object();
