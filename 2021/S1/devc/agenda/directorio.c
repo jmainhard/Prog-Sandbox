ba4f0956cb8630d33da3a6fffdae241618eba53f
Jorge Mainhard Bravo <63559099+jmainhard@users.noreply.github.com>
2021-04-19 00:51:21 -0400

    Bad practices

--- 2021/S1/devc/agenda/directorio.c
+++ 2021/S1/devc/agenda/directorio.c
@@ -8,12 +8,13 @@
 void save_persona(const char *nombre, const char *direccion)
 {
     const char *file_name = "datos_agenda.json";
-    char suffix = nombre[0]; 
+    // FIXME: generar key de manera correcta
+    char *suffix = nombre[0]; 
     char *prefix = "per_";
     json_object *root = json_object_from_file("datos.json");
 
     strcat(prefix, suffix);
-    const char *key = prefix;
+    const char *key = "per_a";
 
     json_object *contactos = json_object_object_get(root, key);
     json_object *new_persona = json_object_new_object();
