f6705ed5c52f4375575fbb0146622d214d4c2752
Jorge Mainhard Bravo <63559099+jmainhard@users.noreply.github.com>
2021-06-29 22:44:24 -0400

    Initial commit javascript/

---
+++ javascript/ejercicios/ejercicios.js
@@ -0,0 +1,119 @@
+// 29-06-2021 Jorge M.
+// Ejercicios Javascript
+// exportando a ./index.html
+// posible fixme: se está modificando el array original en muchas funciones
+
+// 1
+export function biggie(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            arr[i] = 'big';
+        }
+    }
+    return arr;
+}
+
+// 2
+export function printMinReturnMax(arr) {
+    console.log(Math.min(...arr)); // ...spread operator
+    return Math.max(...arr);
+}
+
+// es par
+function isEven(number) {
+    return number % 2 === 0;
+}
+
+// 3
+export function printOneReturnOther(arr) {
+    let penultimo = arr.slice(arr.length - 2 ,arr.length - 1)[0];
+    // fuente: https://javascript.info/array-methods#find-and-findindex
+    let primerImpar = arr.find(element => !isEven(element)); // lambda parecido al de Java (al reves, no sé)
+    console.log("Penúltimo valor: " + penultimo);
+    return primerImpar;
+}
+
+// 4
+export function dobleVision(arr) {
+    let newArray = [];
+    arr.forEach(element => newArray.push(element * 2));
+    return newArray;
+}
+
+// 5
+export function countPositives(arr) {
+    let positivos = 0;
+    arr.forEach(element => {
+        if (element > 0) positivos++;
+    });
+    arr[arr.length - 1] = positivos;
+    return arr;
+}
+
+function initContador() {
+    let contador = {
+        pares: 0,
+        impares: 0,
+        resetPares: function() {
+            this.pares = 0;
+        },
+        resetImpares: function() {
+            this.impares = 0;
+        }
+    };
+    return contador;
+}
+
+// 6
+// Pares e Impares -Crea una función que acepte un array. Cada vez que ese array tenga 3 valores impares seguidos, 
+// imprime (print) “¡Qué imparcial!”, y cada vez que tenga 3 pares seguidos, imprime (print) “¡Es para bien!”.
+
+// impares y pares v2 (refactor v1)
+export function oddAndEven(arr) {
+    let contador = initContador();
+    for (let i = 0; i < arr.length; i++) {
+        if (isEven(arr[i])) {
+            contador.pares++;
+            // console.log('par: ' + contador.pares); // debug, se puede borrar
+            if (contador.pares === 3) {
+                console.log('!Es para bien!');
+                contador.resetPares();
+            }
+            contador.resetImpares();
+        } else {
+            contador.impares++;
+            // console.log('impar: ' + contador.impares);
+            if(contador.impares === 3) {
+                console.log('!Qué imparcial!');
+                contador.resetImpares();
+            }
+            contador.resetPares();
+        }
+    }
+}
+
+// v1
+// export function oddAndEven(arr) {
+//     let pares = 0, impares = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         if (isEven(arr[i])) {
+//             pares++;
+//             if (pares === 3) {
+//                 console.log('!Es para bien!');
+//                 pares = 0;
+//             }
+//             impares = 0;
+//         } else {
+//             impares++;
+//             if(impares === 3) {
+//                 console.log('!Qué imparcial!');
+//                 impares = 0;
+//             }
+//             pares = 0;
+//         }
+//     }
+// }
+
+
+
+
